8efef8a59f49088c992d58c536031da1796bd6c8
nishant <nishant.singh87@gmail.com>
2015-08-22 23:44:06 +0530

    adding loader, and compile app.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    jsHtmlComponents = [
-
+    appCssComponents = [
+      './style/loader.css',
+      './style/properties.css'
     ],
+
     indexHtmlComponents = [
       './pages/index/before.html',
       './pages/index/app.html',
-      './pages/index/after.html'];
+      './pages/index/loader.html',
+      './pages/index/after.html'],
+
+    createAppCss = function () {
+      return gulp.src(appCssComponents)
+          .pipe(concat('app.css'))
+          .pipe(gulp.dest('./style'));
+    },
+
+    createIndexHtml = function () {
+      return gulp.src(indexHtmlComponents)
+          .pipe(concat('index.html'))
+          .pipe(gulp.dest('./'));
+    };
 
 gulp.task('default', function () {
-  return gulp.src(indexHtmlComponents)
-      .pipe(concat('index.html'))
-      .pipe(gulp.dest('./'));
+  createIndexHtml();
+  createAppCss();
 });
