ef7c662cbbb47c975145fb8fc661af281aa744ff
nishant <nishant.singh87@gmail.com>
2015-08-23 23:54:20 +0530

    renaming items to books, on homepage.

--- app/home/home-controller.js
+++ app/home/home-controller.js
@@ -4,6 +4,6 @@
   app.controller("HomeController", ["$scope", "userInterface", "bookStore", function ($scope, userInterface, bookStore) {
     userInterface.hideLoader();
 
-    $scope.items = bookStore.filteredBooks;
+    $scope.books = bookStore.filteredBooks;
   }]);
 }).call(this);
